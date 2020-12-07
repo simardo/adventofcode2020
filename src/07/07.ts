6320b02964556df608954a44a81e2da13fca0848
simardo <simardo@gmail.com>
2020-12-07 09:42:40 -0500

    simplified version

--- src/07/07.ts
+++ src/07/07.ts
@@ -11,8 +11,7 @@ type Bag = {
     content?: [string, number][]
 }
 
-// part 1
-function doPart1(input: string): void {
+function buildBags(input: string): Bags {
     let match: RegExpMatchArray | null;
 
     const bags: Bags = {};
@@ -34,6 +33,13 @@ function doPart1(input: string): void {
         }
         bags[bag] = b;
     }
+
+    return bags;
+}
+
+// part 1
+function doPart1(input: string): void {
+    const bags: Bags = buildBags(input);
 
     const hasShinyBag: (b: Bag) => boolean =
         b => b.content !== undefined && (b.content.find(f => f[0] === 'shiny gold') !== undefined || b.content!.some(bb => hasShinyBag(bags[bb[0]])));
@@ -47,27 +53,7 @@ doPart1(INPUT);
 
 // part 2
 function doPart2(input: string): void {
-    let match: RegExpMatchArray | null;
-
-    const bags: Bags = {};
-
-    while ((match = mainRx.exec(input)) !== null) {
-        let [, bag, content] = match;
-
-        const b: Bag = {
-            color: bag
-        }
-
-        content = content.substr(0, content.length - 1);
-        if (content !== 'no other bags') {
-            const cinfo: [string, number][] = content.split(',').map(s => {
-                const [, qty, color] = s.trim().match(contentRx)!;
-                return [color, Number.parseInt(qty)];
-            });
-            b.content = cinfo;
-        }
-        bags[bag] = b;
-    }
+    const bags: Bags = buildBags(input);
 
     const contains: (b: Bag) => number = b => b.content !== undefined ? b.content.reduce((a,bb) => a + (bb[1] + bb[1] * contains(bags[bb[0]])), 0) : 0;
 
