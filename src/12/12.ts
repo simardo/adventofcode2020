961e4034fcce9b65ef2fbf3f1a994dc21365286c
simardo <simardo@gmail.com>
2020-12-12 10:21:06 -0500

    refactor

--- src/12/12.ts
+++ src/12/12.ts
@@ -2,61 +2,49 @@ import { INPUT, TEST_1 } from './input.ts';
 
 console.log('12 décembre');
 
+type MoveFn = (d: number) => void;
+type MovesFn = [MoveFn, MovesFn | undefined, MovesFn | undefined];
+
 // part 1
 function doPart1(input: string): void {
     const directions: [string, number][] = input.split('\n').map(d => [d[0], Number.parseInt(d.substr(1))]);
 
     let coord: [number, number] = [0, 0];
 
-    const moveEast = (d: number) => coord[0] = coord[0] + d;
-    const moveNorth = (d: number) => coord[1] = coord[1] - d;
-    const moveWest = (d: number) => coord[0] = coord[0] - d;
-    const moveSouth = (d: number) => coord[1] = coord[1] + d;
+    const moveEast: MovesFn = [(d: number) => coord[0] = coord[0] + d, undefined, undefined];
+    const moveNorth: MovesFn = [(d: number) => coord[1] = coord[1] - d, undefined, undefined];
+    const moveWest: MovesFn = [(d: number) => coord[0] = coord[0] - d, undefined, undefined];
+    const moveSouth: MovesFn = [(d: number) => coord[1] = coord[1] + d, undefined, undefined];
+
+    moveEast[1] = moveNorth;
+    moveEast[2] = moveSouth;
+    moveNorth[1] = moveWest;
+    moveNorth[2] = moveEast;
+    moveWest[1] = moveSouth;
+    moveWest[2] = moveNorth;
+    moveSouth[1] = moveEast;
+    moveSouth[2] = moveWest;
 
     let main = moveEast;
 
     directions.forEach(d => {
         const [dir, num] = d;
         if (dir === 'F') {
-            main(num);
+            main[0](num);
         } else if (dir === 'N') {
-            moveNorth(num);
+            moveNorth[0](num);
         } else if (dir === 'S') {
-            moveSouth(num);
+            moveSouth[0](num);
         } else if (dir === 'E') {
-            moveEast(num);
+            moveEast[0](num);
         } else if (dir === 'W') {
-            moveWest(num);
+            moveWest[0](num);
         } else if ((dir === 'L' && num === 90) || (dir === 'R' && num === 270)) {
-            if (main === moveEast) {
-                main = moveNorth;
-            } else if (main === moveNorth) {
-                main = moveWest;
-            } else if (main === moveWest) {
-                main = moveSouth;
-            } else if (main === moveSouth) {
-                main = moveEast;
-            }
-        }else if (num === 180) {
-            if (main === moveEast) {
-                main = moveWest;
-            } else if (main === moveNorth) {
-                main = moveSouth;
-            } else if (main === moveWest) {
-                main = moveEast;
-            } else if (main === moveSouth) {
-                main = moveNorth;
-            }
-        }else if ((dir === 'R' && num === 90) || (dir === 'L' && num === 270)) {
-            if (main === moveEast) {
-                main = moveSouth;
-            } else if (main === moveNorth) {
-                main = moveEast;
-            } else if (main === moveWest) {
-                main = moveNorth;
-            } else if (main === moveSouth) {
-                main = moveWest;
-            }
+            main = main[1]!;
+        } else if (num === 180) {
+            main = main[1]![1]!;
+        } else if ((dir === 'R' && num === 90) || (dir === 'L' && num === 270)) {
+            main = main[2]!;
         }
     });
 
@@ -70,8 +58,8 @@ doPart1(INPUT);
 function doPart2(input: string): void {
     const directions: [string, number][] = input.split('\n').map(d => [d[0], Number.parseInt(d.substr(1))]);
 
-    let waypoint = [10,-1];
-    let coord = [0,0];
+    let waypoint = [10, -1];
+    let coord = [0, 0];
 
     const moveEast = (d: number) => waypoint[0] = waypoint[0] + d;
     const moveNorth = (d: number) => waypoint[1] = waypoint[1] - d;
@@ -92,15 +80,15 @@ function doPart2(input: string): void {
         } else if (dir === 'W') {
             moveWest(num);
         } else if ((dir === 'L' && num === 90) || (dir === 'R' && num === 270)) {
-            const [e,n] = waypoint;
+            const [e, n] = waypoint;
             waypoint[0] = n;
             waypoint[1] = -e;
-        }else if (num === 180) {
-            const [e,n] = waypoint;
+        } else if (num === 180) {
+            const [e, n] = waypoint;
             waypoint[0] = -e;
             waypoint[1] = -n;
-        }else if ((dir === 'R' && num === 90) || (dir === 'L' && num === 270)) {
-            const [e,n] = waypoint;
+        } else if ((dir === 'R' && num === 90) || (dir === 'L' && num === 270)) {
+            const [e, n] = waypoint;
             waypoint[0] = -n;
             waypoint[1] = e;
         }
