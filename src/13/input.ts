20fd9c823bf0fc7966e49268d6f22caab79f3abf
simardo <simardo@gmail.com>
2020-12-13 23:00:02 -0500

    day 13

---
+++ src/13/input.ts
@@ -0,0 +1,19 @@
+export const TEST_1: string =
+    `939
+7,13,x,x,59,x,31,19`;
+
+export const TEST_2: string =
+    `0
+17,x,13,19`;
+
+export const TEST_3: string =
+    `0
+67,7,59,61`;
+
+export const TEST_4: string =
+    `0
+1789,37,47,1889`;
+
+export const INPUT: string =
+    `1006401
+17,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,449,x,x,x,x,x,x,x,23,x,x,x,x,13,x,x,x,x,x,19,x,x,x,x,x,x,x,x,x,x,x,607,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29`;
