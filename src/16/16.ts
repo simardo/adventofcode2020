c438990ff0131ab8f27725f24beadc3ec0d04703
simardo <simardo@gmail.com>
2020-12-16 18:00:59 -0500

    refactor

--- src/16/16.ts
+++ src/16/16.ts
@@ -6,29 +6,17 @@ const rx: RegExp = /(.+)?:\s(\d+\-\d+)\sor\s(\d+\-\d+)/;
 
 // part 1
 function doPart1(input: string): void {
-    const doc: string[] = input.split('\n');
+    const [classesIn, /* skip ticket */, nearbyIn] = input.split('\n\n');
 
-    let ticket: number[] | null | undefined;
-    let tickets: number[][] = [];
     const ranges: [string, [number, number], [number, number]][] = [];
-
-    doc.filter(l => l).forEach(line => {
-        const match = line.match(rx);
-        if (match) {
-            const [, field, range1, range2] = match;
-            const r1: number[] = range1.split('-').map(r => Number.parseInt(r));
-            const r2: number[] = range2.split('-').map(r => Number.parseInt(r));
-            ranges.push([field, [r1[0], r1[1]], [r2[0], r2[1]]])
-        } else if (line === 'your ticket:') {
-            ticket = null;
-        } else if (line === 'nearby tickets:') {
-            //
-        } else if (ticket === null) {
-            ticket = line.split(',').map(n => Number.parseInt(n));
-        } else {
-            tickets.push(line.split(',').map(n => Number.parseInt(n)))
-        }
+    classesIn.split('\n').forEach(line => {
+        const [, field, range1, range2] = line.match(rx)!;
+        const r1: number[] = range1.split('-').map(r => Number.parseInt(r));
+        const r2: number[] = range2.split('-').map(r => Number.parseInt(r));
+        ranges.push([field, [r1[0], r1[1]], [r2[0], r2[1]]])
     });
+
+    const tickets: number[][] = nearbyIn.split('\n').slice(1).map(nl => nl.split(',').map(n => Number.parseInt(n)));
 
     const inRange: (n: number) => boolean = (n => ranges.some(r => {
         const [, range1, range2] = r;
@@ -44,35 +32,22 @@ function doPart1(input: string): void {
 
 // doPart1(TEST_1);
 // doPart1(TEST_2);
-// doPart1(TEST_3);
-// doPart1(TEST_4);
 doPart1(INPUT);
 
 // part 2
 function doPart2(input: string): void {
-    const doc: string[] = input.split('\n');
+    const [classesIn, ticketIn, nearbyIn] = input.split('\n\n');
 
-    let ticket: number[] | null | undefined;
-    let tickets: number[][] = [];
     const ranges: [string, [number, number], [number, number]][] = [];
-
-    doc.filter(l => l).forEach(line => {
-        const match = line.match(rx);
-        if (match) {
-            const [, field, range1, range2] = match;
-            const r1: number[] = range1.split('-').map(r => Number.parseInt(r));
-            const r2: number[] = range2.split('-').map(r => Number.parseInt(r));
-            ranges.push([field, [r1[0], r1[1]], [r2[0], r2[1]]])
-        } else if (line === 'your ticket:') {
-            ticket = null;
-        } else if (line === 'nearby tickets:') {
-            // tickets = [];
-        } else if (ticket === null) {
-            ticket = line.split(',').map(n => Number.parseInt(n));
-        } else {
-            tickets.push(line.split(',').map(n => Number.parseInt(n)))
-        }
+    classesIn.split('\n').forEach(line => {
+        const [, field, range1, range2] = line.match(rx)!;
+        const r1: number[] = range1.split('-').map(r => Number.parseInt(r));
+        const r2: number[] = range2.split('-').map(r => Number.parseInt(r));
+        ranges.push([field, [r1[0], r1[1]], [r2[0], r2[1]]])
     });
+
+    const ticket: number[] = ticketIn.split('\n')[1].split(',').map(n => Number.parseInt(n));
+    const tickets: number[][] = nearbyIn.split('\n').slice(1).map(nl => nl.split(',').map(n => Number.parseInt(n)));
 
     const inRange: (n: number) => boolean = (n => ranges.some(r => {
         const [, range1, range2] = r;
@@ -100,24 +75,15 @@ function doPart2(input: string): void {
     });
 
     while (!classes.every(c => c.size === 1)) {
-        classes.forEach((c, i) => {
-            if (c.size === 1) {
-                classes.forEach((cc, ii) => {
-                    if (ii !== i) {
-                        const v: string = c.values().next().value;
-                        cc.delete(v);
-                    }
-                });
-            }
-        });
+        classes.filter(c => c.size === 1).forEach(c =>
+            classes.filter(cc => cc !== c).forEach(cc =>
+                cc.delete(c.values().next().value))
+        );
     }
 
     const r = ticket!.reduce((m, f, i) => (classes[i].values().next().value as string).startsWith('departure') ? m * f : m, 1);
     console.log(r);
 }
 
-// doPart2(TEST_1);
 // doPart2(TEST_2);
-// doPart2(TEST_3);
-// doPart2(TEST_4);
 doPart2(INPUT);
